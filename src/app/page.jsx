566223d807d6e25558caf00114ee51d45460d3b9
Hamdi Taha <hamdi.taha@esprit.tn>
2024-02-10 21:29:36 +0100

    navbarr

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,10 @@
+import Navbar from "@/components/navbar/Navbar";
 import styles from "./homepage.module.css";
 import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Link href="/">
-        <h1>Ttile</h1>
-        ljdjkfdofhzjfjzbjizbizeeifzoifozeifzef
-        ljdjkfdofhzjfjzbjizbizeeifzoifozeifzef
-        ljdjkfdofhzjfjzbjizbizeeifzoifozeifzef
-        ljdjkfdofhzjfjzbjizbizeeifzoifozeifzef
-        ljdjkfdofhzjfjzbjizbizeeifzoifozeifzef
-        ljdjkfdofhzjfjzbjizbizeeifzoifozeifzef
-
-      </Link>
+      <Navbar />
     </div>
   )
 }
